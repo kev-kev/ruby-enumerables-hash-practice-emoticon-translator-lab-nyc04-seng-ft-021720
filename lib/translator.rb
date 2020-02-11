3310ba4da59af6687ccbdb295b76864c37b0277d
Kevin Clark <kevin.clark93@gmail.com>
2020-02-11 20:00:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,24 @@
-# require modules here
+require 'yaml'
+require 'pp'
 
-def load_library
-  # code goes here
+def load_library(path)
+  pp YAML.load_file(path)
+  result = {'get_meaning' => {}, 'get_emoticon' => {}}
+  path.each{ |meaning, emoticon_arr|
+    result['get_meaning'] = meaning
+    result['get_emoticon'] = emoticon_arr[1]
+  }
+  result
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(western_emoticon)
+  load_library.each{ |meaning, arr|
+    if arr[0] == western_emoticon
+      return arr[1]
+    end
+  }
 end
 
-def get_english_meaning
+def get_english_meaning(japanese_emoticon)
   # code goes here
 end
